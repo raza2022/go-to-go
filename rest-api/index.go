f28caf0858f3d7c6039cb398476b00950e14746a
Abdul Hameed <raza2022@gmail.com>
2018-09-04 19:15:51 +0500

    API all endpoints tested successfully :rocket:

--- rest-api/index.go
+++ rest-api/index.go
@@ -4,6 +4,7 @@ import (
 	"dao"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
 )
@@ -22,27 +23,78 @@ type Config struct {
 	Database string
 }
 
-var configg = Config{"localhost", "movies_db"}
-var daoo = dao.MoviesDAO{"localhost", "movies_db"}
+var config = Config{"localhost", "movies_db"}
+var daoRef = dao.MoviesDAO{"localhost", "movies_db"}
 
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	respondWithJSON(w, code, map[string]string{"error": msg})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
-}
-
+// GET list of movies
 func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
-	movies, err := daoo.FindAll()
+	movies, err := daoRef.FindAll()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 	respondWithJson(w, http.StatusOK, movies)
+}
+
+// GET a movie by its ID
+func FindMovieEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	movie, err := daoRef.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Movie ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, movie)
+}
+
+// POST a new movie
+func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var movie dao.Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	movie.ID = bson.NewObjectId()
+	if err := daoRef.Insert(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusCreated, movie)
+}
+
+// PUT update an existing movie
+func UpdateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var movie dao.Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := daoRef.Update(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+// DELETE an existing movie
+func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var movie dao.Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := daoRef.Delete(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
 }
 
 func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
@@ -53,10 +105,9 @@ func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 }
 
 func init() {
-	//config.Read()
-	daoo.Server = configg.Server
-	daoo.Database = configg.Database
-	daoo.Connect()
+	daoRef.Server = config.Server
+	daoRef.Database = config.Database
+	daoRef.Connect()
 }
 
 func main() {
