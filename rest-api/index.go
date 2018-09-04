13672d8b19a70d1d7ccb52d5d31a8d17a451b7b7
Abdul Hameed <raza2022@gmail.com>
2018-09-04 19:18:03 +0500

    typo fixed :fire:

--- rest-api/index.go
+++ rest-api/index.go
@@ -113,6 +113,10 @@ func init() {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/movies", AllMoviesEndPoint).Methods("GET")
+	r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
+	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
+	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", FindMovieEndpoint).Methods("GET")
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal(err)
 	}
